a0fc812d078a00d66c1b5199406006e9a69a7b97
Daniel Frey <freydan3@students.zhaw.ch>
2019-09-09 20:28:36 +0200

    mehrere Templates erstellt...

--- app/src/main/java/com/example/barcode_interpreter/BcItem.java
+++ app/src/main/java/com/example/barcode_interpreter/BcItem.java
@@ -16,32 +16,4 @@ public class BcItem {
         itemDim = itDim;
     }
 
-    public ArrayList<BcItem> CreateSilhouet() {
-        ArrayList<BcItem> template_1 = new ArrayList<>();
-        BcItem item;
-
-        item = new BcItem ("JobID", 1, 8, "");
-        template_1.add(item);
-        item = new BcItem ("Endsheet height", 9, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Book bloc thickness", 13, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Book bloc height", 17, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Cut Off Length", 21, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Final Height", 25, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Final Width", 29, 4, "mm");
-        template_1.add(item);
-        item = new BcItem ("Last sheet indicator", 33, 2, "");
-        template_1.add(item);
-        item = new BcItem ("Current sheet indicator", 35, 3, "");
-        template_1.add(item);
-        item = new BcItem ("Total sheets", 38, 3, "");
-        template_1.add(item);
-
-        return template_1;
-    }
-
 }
